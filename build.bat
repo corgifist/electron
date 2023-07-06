c2a574149db5d56799c7022aaba0b8012da654d0
corgifist <86985149+corgifist@users.noreply.github.com>
2023-07-06 18:26:05 +0300

    (Cross-platform ?) compiler path

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 taskkill /IM Electron.exe
 rm Electron.exe
 call generate_headers.bat
-cmake . -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++
+cmake . -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++
 make
 python gen_batch.py project_configuration_impl
 call compile_impl.bat
