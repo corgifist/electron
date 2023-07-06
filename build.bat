de63d0878b2ce063b6d204b4c2a42e3c8874ec94
corgifist <86985149+corgifist@users.noreply.github.com>
2023-07-06 18:24:26 +0300

    Trying to fix some compiler issues

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 taskkill /IM Electron.exe
 rm Electron.exe
 call generate_headers.bat
-cmake .
+cmake . -DCMAKE_C_COMPILER=/usr/bin/gcc -DCMAKE_CXX_COMPILER=/usr/bin/g++
 make
 python gen_batch.py project_configuration_impl
 call compile_impl.bat
