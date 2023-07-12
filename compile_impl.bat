1451a07d0a110dffd82aff71fd542a4d98dd7019
corgifist <86985149+corgifist@users.noreply.github.com>
2023-07-12 08:03:14 +0000

    Delete compile_impl.bat

--- compile_impl.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-g++ -c impl/render_preview_impl.cpp -I src/ -g -fPIC -m64 -DELECTRON_IMPLEMENTATION_MODE
-g++ -shared -o render_preview_impl.dll render_preview_impl.o -lglfw3 -lopengl32 -fPIC -g -mwindows -L. -m64
