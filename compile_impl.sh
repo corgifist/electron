095ce919a66fa71526e34c67e02f979d4d7190e1
corgifist <86985149+corgifist@users.noreply.github.com>
2023-07-21 18:52:18 +0000

    Delete compile_impl.sh

--- compile_impl.sh
+++
@@ -1,3 +0,0 @@
-g++ -c impl/sdf2d_layer.cpp -I src/ -g -fPIC -m64 -DELECTRON_IMPLEMENTATION_MODE -std=c++20
-g++ -shared -o libsdf2d_layer.so sdf2d_layer.o  -lstdc++ -ldl -fPIC -g -L. -m64
-rm sdf2d_layer.o
